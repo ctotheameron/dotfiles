3952c3898c24e418e6150203448c2ae102c75c56
Cameron Austgen <3002968+ctotheameron@users.noreply.github.com>
2026-06-10 17:18:19 +0800

    nvim: show 80-column ruler

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Show a vertical ruler at column 80
+vim.opt.colorcolumn = "80"
