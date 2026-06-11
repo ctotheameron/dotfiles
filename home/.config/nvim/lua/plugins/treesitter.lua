75e6479b1bf962a936adc23ac733d4f768bce1f2
Cameron Austgen <3002968+ctotheameron@users.noreply.github.com>
2026-06-11 11:55:10 +0800

    nvim: add rainbow-delimiters for color-matched brackets

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -3,4 +3,10 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = { ensure_installed = { "graphql" } },
   },
+
+  -- Rainbow brackets: color-match nested delimiters
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    event = "LazyFile",
+  },
 }
