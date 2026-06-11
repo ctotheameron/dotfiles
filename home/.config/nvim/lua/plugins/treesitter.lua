ac990e6b68ceb9a52c7159ef3e58f32514784b01
Cameron Austgen <3002968+ctotheameron@users.noreply.github.com>
2026-06-11 16:08:13 +0800

    nvim: drop rainbow-delimeters

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -3,10 +3,4 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = { ensure_installed = { "graphql" } },
   },
-
-  -- Rainbow brackets: color-match nested delimiters
-  {
-    "HiPhish/rainbow-delimiters.nvim",
-    event = "LazyFile",
-  },
 }
